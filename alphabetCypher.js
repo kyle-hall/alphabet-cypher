0cd99190082990cc467276635ae3f132c2cec8df
Kyle Hall <khall@pillartechnology.com>
2019-06-06 13:33:35 -0500

    refactor cypher to be more functional

--- alphabetCypher.js
+++ alphabetCypher.js
@@ -4,12 +4,11 @@ import keyRepeater from './keyRepeater/keyRepeater'
 const Cypher = {
   encode: (key, msg) => {
     const repeatedKey = keyRepeater.generateRepeatedKey(key, msg)
-    let encodedMessage = ''
-    for (let i = 0; i < msg.length; i++) {
-      encodedMessage = encodedMessage + substitutionChart[repeatedKey[i]][msg[i]]
-    }
 
-    return encodedMessage
+    return repeatedKey
+      .split('')
+      .map((x, index) => substitutionChart[x][msg[index]])
+      .join('')
   }
 }
 
--- keyRepeater/keyRepeater.js
+++ keyRepeater/keyRepeater.js
@@ -4,6 +4,7 @@ const generateRepeatedKey = (key, msg) => {
   const loop = (result, currentKey, theMessage) => {
     if (theMessage.length == 0) return result
 
+    // TODO: Fix this because it's not actually all that functional
     if (currentKey.length == 0) currentKey = key
     return loop(result + currentKey[0], currentKey.slice(1), theMessage.slice(1))
   }
